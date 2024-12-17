16e7171d8f4ee552521f2f3bb845673e6bebdbb6
Javier <javierygnacio619@gmail.com>
2024-12-17 19:15:04 -0300

    Menu desplegable

--- src/Components/NavCategories.jsx
+++ src/Components/NavCategories.jsx
@@ -1,10 +1,45 @@
+import React, { useState } from "react";
+
 import './../Styles/NavCategories.css';
+
 export const NavCategories = () => {
+    const [activeMenu, setActiveMenu] = useState(null);
+
+    const mostrarMenu = () => {
+        setActiveMenu(!activeMenu)
+    }
+
+
     return (
         <nav className="navbarC">
             <div className="navbar-items">
                 <p>Ofertas</p>
-                <p>Nuevos</p>
+                <p onClick={mostrarMenu}
+                >Nuevos</p>
+                {activeMenu && (
+                    <div className="dropdown-menu"
+                    onMouseLeave={mostrarMenu}>
+                        <div className="dropdown-section">
+                            <h4>Top Brands</h4>
+                            <p>Nike</p>
+                            <p>adidas</p>
+                            <p>Fear of God</p>
+                            <p>Supreme</p>
+                            <p>Crocs</p>
+                            <p>BAPE</p>
+                            <p>Converse</p>
+                        </div>
+                        <div className="dropdown-section">
+                            <h4>Trending Brands</h4>
+                            <p>Kith</p>
+                            <p>Pop Mart</p>
+                            <p>Denim Tears</p>
+                            <p>Balenciaga</p>
+                            <p>Louis Vuitton</p>
+                            <p>Gucci</p>
+                        </div>
+                    </div>
+                )}
                 <p>Hombres</p>
                 <p>Mujeres</p>
                 <p>Niños</p>
@@ -12,5 +47,8 @@ export const NavCategories = () => {
                 <p>Accesorios</p>
             </div>
         </nav>
+
     );
 }
+
+
